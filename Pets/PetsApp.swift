a3baf98553db30634afeb0ca8c00028b472b53be
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-05-17 17:25:25 -0600

    Getting Started

---
+++ Pets/PetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetsApp.swift
+//  Pets
+//
+//  Created by Marco Alonso Rodriguez on 17/05/23.
+//
+
+import SwiftUI
+
+@main
+struct PetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
