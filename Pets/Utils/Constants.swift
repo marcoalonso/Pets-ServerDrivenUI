5f35bc5f8220befdb72b2037e14fa3971e667467
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-05-17 17:32:52 -0600

    11 Implementing Webservice

---
+++ Pets/Utils/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Pets
+//
+//  Created by Marco Alonso Rodriguez on 17/05/23.
+//
+
+import Foundation
+
+struct Constants {
+    struct Urls {
+        static let baseUrl = "http://localhost:3000/"
+        static let petListing = "\(baseUrl)/pet-listing"
+    }
+}
